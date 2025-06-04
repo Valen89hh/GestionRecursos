feb7d2f2d93536dac297625b8e6ae6e9fdca30c8
valentino <126835108+Valen89hh@users.noreply.github.com>
2025-06-03 22:35:13 -0500

    add tail

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    base: process.env.ASSET_URL ? new URL(process.env.ASSET_URL).pathname : '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
